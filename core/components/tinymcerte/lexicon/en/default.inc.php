63038c7bec5360ba41c8d2d4443f899a7070044f
Jan Peca <pecajan@gmail.com>
2015-02-16 16:53:32 +0100

    Settings translations

--- core/components/tinymcerte/lexicon/en/default.inc.php
+++ core/components/tinymcerte/lexicon/en/default.inc.php
@@ -7,3 +7,35 @@
  */
 
 $_lang['tinymcerte'] = 'TinyMCE Rich Text Editor';
+
+$_lang['area_tinymce.style_formats'] = 'Style formats';
+$_lang['area_tinymce.toolbar'] = 'Toolbar';
+
+$_lang['setting_tinymcerte.plugins'] = 'Plugins';
+$_lang['setting_tinymcerte.plugins_desc'] = 'Set what plugins should be included, by default, NO plugins are loaded. <a href="http://www.tinymce.com/wiki.php/Configuration:plugins">http://www.tinymce.com/wiki.php/Configuration:plugins</a>';
+$_lang['setting_tinymcerte.toolbar1'] = 'Toolbar 1';
+$_lang['setting_tinymcerte.toolbar1_desc'] = 'This controls what buttons you want show up in the toolbar. <a href="http://www.tinymce.com/wiki.php/Configuration:toolbar%3CN%3E">http://www.tinymce.com/wiki.php/Configuration:toolbar%3CN%3E</a>';
+$_lang['setting_tinymcerte.toolbar2'] = 'Toolbar 2';
+$_lang['setting_tinymcerte.toolbar2_desc'] = 'This controls what buttons you want show up in the toolbar. <a href="http://www.tinymce.com/wiki.php/Configuration:toolbar%3CN%3E">http://www.tinymce.com/wiki.php/Configuration:toolbar%3CN%3E</a>';
+$_lang['setting_tinymcerte.toolbar3'] = 'Toolbar 3';
+$_lang['setting_tinymcerte.toolbar3_desc'] = 'This controls what buttons you want show up in the toolbar. <a href="http://www.tinymce.com/wiki.php/Configuration:toolbar%3CN%3E">http://www.tinymce.com/wiki.php/Configuration:toolbar%3CN%3E</a>';
+$_lang['setting_tinymcerte.menubar'] = 'Menubar';
+$_lang['setting_tinymcerte.menubar_desc'] = 'This option allows you to configure the menus you want to appear in the menu bar. <a href="http://www.tinymce.com/wiki.php/Configuration:menubar">http://www.tinymce.com/wiki.php/Configuration:menubar</a>';
+$_lang['setting_tinymcerte.statusbar'] = 'Statusbar';
+$_lang['setting_tinymcerte.statusbar_desc'] = 'Boolean, show or hide the statusbar. <a href="http://www.tinymce.com/wiki.php/Configuration:statusbar">http://www.tinymce.com/wiki.php/Configuration:statusbar</a>';
+$_lang['setting_tinymcerte.image_advtab'] = 'Image advanced tab';
+$_lang['setting_tinymcerte.image_advtab_desc'] = 'This option enables the advanced tab the image dialog allowing you to add custom styles to images. <a href="http://www.tinymce.com/wiki.php/Configuration:image_advtab">http://www.tinymce.com/wiki.php/Configuration:image_advtab</a>';
+$_lang['setting_tinymcerte.object_resizing'] = 'Object resizing';
+$_lang['setting_tinymcerte.object_resizing_desc'] = 'This options allows you to turn on/off the resizing handles on images, tables or media objects. <a href="http://www.tinymce.com/wiki.php/Configuration:object_resizing">http://www.tinymce.com/wiki.php/Configuration:object_resizing</a>';
+$_lang['setting_tinymcerte.style_formats'] = 'Style formats';
+$_lang['setting_tinymcerte.style_formats_desc'] = 'This option enables you to add more advanced style formats for text and other elements to the editor. <a href="http://www.tinymce.com/wiki.php/Configuration:style_formats">http://www.tinymce.com/wiki.php/Configuration:style_formats</a>';
+$_lang['setting_tinymcerte.headers_format'] = 'Headers format';
+$_lang['setting_tinymcerte.headers_format_desc'] = 'This option enables you to add more advanced style formats for text and other elements to the editor. <a href="http://www.tinymce.com/wiki.php/Configuration:style_formats">http://www.tinymce.com/wiki.php/Configuration:style_formats</a>';
+$_lang['setting_tinymcerte.inline_format'] = 'Inline format';
+$_lang['setting_tinymcerte.inline_format_desc'] = 'This option enables you to add more advanced style formats for text and other elements to the editor. <a href="http://www.tinymce.com/wiki.php/Configuration:style_formats">http://www.tinymce.com/wiki.php/Configuration:style_formats</a>';
+$_lang['setting_tinymcerte.blocks_format'] = 'Blocks format';
+$_lang['setting_tinymcerte.blocks_format_desc'] = 'This option enables you to add more advanced style formats for text and other elements to the editor. <a href="http://www.tinymce.com/wiki.php/Configuration:style_formats">http://www.tinymce.com/wiki.php/Configuration:style_formats</a>';
+$_lang['setting_tinymcerte.alignment_format'] = 'Alignment format';
+$_lang['setting_tinymcerte.alignment_format_desc'] = 'This option enables you to add more advanced style formats for text and other elements to the editor. <a href="http://www.tinymce.com/wiki.php/Configuration:style_formats">http://www.tinymce.com/wiki.php/Configuration:style_formats</a>';
+$_lang['setting_tinymcerte.paste_as_text'] = 'Paste as text';
+$_lang['setting_tinymcerte.paste_as_text_desc'] = 'This option enables you to set the default state of the "paste as text" edit menu option. <a href="http://www.tinymce.com/wiki.php/Configuration:paste_as_text">http://www.tinymce.com/wiki.php/Configuration:paste_as_text</a>';
