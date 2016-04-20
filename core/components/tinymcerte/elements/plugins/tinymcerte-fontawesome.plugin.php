f99ff42f39a2100ac0120033834a75298171594c
Luca <luca.carnevale@unifr.ch>
2016-04-20 16:17:27 +0200

    Added the necessary elements that allow the integration of FontAweasome Plugin ( https://github.com/josh18/TinyMCE-FontAwesome-Plugin.git ) .
    This edition was described in the issue #37 , tested on a local install but not packaged .

---
+++ core/components/tinymcerte/elements/plugins/tinymcerte-fontawesome.plugin.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * TinyMCE Rich Tech Editor integration FontAwesome plugin
+ *
+ * This plugin register the font-awesome css in the manager
+ *
+ * original plugin credit https://github.com/josh18/TinyMCE-FontAwesome-Plugin
+ *
+ */
+
+
+switch ($modx->event->name) {
+    case 'OnManagerPageBeforeRender':
+        $cssurl = $modx->getOption('tinymcerte.content_css', null, "//netdna.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css");
+        $modx->regClientCSS("//netdna.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css");
+        break;
+}
