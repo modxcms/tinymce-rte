8f452b0ed7bd7b92ab74df316c48b61d9becb1ab
OptimusCrime <thomasgautv@hotmail.com>
2016-11-23 21:44:10 +0100

    Add support to filter resources from other contexts when creating internal links

--- assets/components/tinymcerte/js/vendor/tinymce/plugins/modxlink/search.php
+++ assets/components/tinymcerte/js/vendor/tinymce/plugins/modxlink/search.php
@@ -11,11 +11,28 @@ require_once MODX_CONNECTORS_PATH.'index.php';
 $query = $modx->getOption('q',$_REQUEST,'');
 if(strlen($query) < 3) exit();
 
+// System setting defines if we should list links across contexts or not
+$across_contexts = $modx->getOption('tinymcerte.links_across_contexts', null, true);
+
 $c = $modx->newQuery('modResource');
-$c->where(array(
-    'pagetitle:LIKE' => '%'.$query.'%',
-    'OR:alias:LIKE' => '%'.$query.'%',
+
+// Note, reason we nest the query in two array is because if we are going to filter on context, we want the final query on the form:
+// ((pagetitle or alias) and context). If we don't filter on context, it will make no difference that the where clause has an extra
+// parenthesis
+$where_clause = array(array(
+  'pagetitle:LIKE' => '%'.$query.'%',
+  'OR:alias:LIKE' => '%'.$query.'%',
 ));
+
+if (!$across_contexts) {
+  if (isset($_GET['ctx']) and strlen($_GET['ctx'])) {
+	$where_clause[] = array(
+      'context_key' => $_GET['ctx']
+	);
+  }
+}
+
+$c->where($where_clause);
 
 $count = $modx->getCount('modResource',$c);
 
@@ -23,6 +40,7 @@ $c->select(array('id','pagetitle','alias'));
 $c->limit(10);
 
 $resources = $modx->getCollection('modResource',$c);
+
 $a = array();
 foreach ($resources as $resource) {
 	$a[] = array(
