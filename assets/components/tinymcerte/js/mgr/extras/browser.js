a315ecea876595d48012f2def7ec676dc0554f6f
Mat Dave Jones <mat@matdave.com>
2017-11-09 12:17:43 -0600

    Test if returned data is null
    If data was null it would fail with "Cannot find property fullRelativeURL of null." Part 1 of solving issue #10

--- assets/components/tinymcerte/js/mgr/extras/browser.js
+++ assets/components/tinymcerte/js/mgr/extras/browser.js
@@ -1,5 +1,6 @@
 Ext.ns('TinyMCERTE');
 TinyMCERTE.browserCallback = function(data) {
-    top.tinymce.activeEditor.windowManager.getParams().oninsert(data.fullRelativeUrl);
+    if(data !== null)
+        top.tinymce.activeEditor.windowManager.getParams().oninsert(data.fullRelativeUrl);
     top.tinymce.activeEditor.windowManager.close()
 };
