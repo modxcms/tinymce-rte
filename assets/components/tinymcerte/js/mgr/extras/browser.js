db0adeeb88e9d1b44d4f6544f1f59a656064ad34
Mat Dave Jones <mat@matdave.com>
2017-11-16 10:16:47 -0600

    Fix Code Formatting
    Changing code formatting based on Jako request.

--- assets/components/tinymcerte/js/mgr/extras/browser.js
+++ assets/components/tinymcerte/js/mgr/extras/browser.js
@@ -1,6 +1,7 @@
 Ext.ns('TinyMCERTE');
 TinyMCERTE.browserCallback = function(data) {
-    if(data !== null)
+    if(data){
         top.tinymce.activeEditor.windowManager.getParams().oninsert(data.fullRelativeUrl);
+    }
     top.tinymce.activeEditor.windowManager.close()
 };
