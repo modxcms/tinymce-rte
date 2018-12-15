56988f159b9bee132d3dac63937ce3af2be075fe
Thomas Jakobi <thomas.jakobi@partout.info>
2018-12-15 16:30:14 +0100

    Make the entity_encoding configurable
    Make the entity_encoding configurable by a system setting. To set the entity_encoding to raw, there has to be a system setting `tinymcerte.entity_encoding` that is set to `raw`.

--- core/components/tinymcerte/model/tinymcerte/events/tinymcerteonrichtexteditorinit.class.php
+++ core/components/tinymcerte/model/tinymcerte/events/tinymcerteonrichtexteditorinit.class.php
@@ -67,6 +67,7 @@ class TinyMCERTEOnRichTextEditorInit extends TinyMCERTEPlugin {
             'skin' => $this->tinymcerte->getOption('skin', array(), 'modx'),
             'relative_urls' => $this->tinymcerte->getOption('relative_urls', array(), true) == 1,
             'remove_script_host'=> $this->tinymcerte->getOption('remove_script_host', array(), true) == 1,
+            'entity_encoding'=>$this->tinymcerte->getOption('entity_encoding', array(), 'named'),
         ), $this->getProperties());
 
         $styleFormats = $this->tinymcerte->getOption('style_formats', array(), '[]');
