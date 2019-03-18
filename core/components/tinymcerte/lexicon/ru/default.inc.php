6321d87e1c31044425dec336b782a34ff0985794
Bochkarev Ivan <ivanx86@gmail.com>
2019-03-19 04:18:33 +0600

    Add russian translate

---
+++ core/components/tinymcerte/lexicon/ru/default.inc.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Default Russian Lexicon Entries for TinyMCE Rich Text Editor
+ *
+ * @package tinymcerte
+ * @subpackage lexicon
+ */
+
+$_lang['tinymcerte'] = 'TinyMCE Rich Text Editor';
+
+$_lang['area_tinymcerte.style_formats'] = 'Форматы стилей';
+$_lang['area_tinymcerte.toolbar'] = 'Панель инструментов';
+$_lang['area_default'] = 'По умолчанию';
+
+$_lang['setting_tinymcerte.plugins'] = 'Плагины';
+$_lang['setting_tinymcerte.plugins_desc'] = 'Укажите, какие плагины должны быть включены, по умолчанию они не загружаются. <a href="http://www.tinymce.com/wiki.php/Configuration:plugins">http://www.tinymce.com/wiki.php/Configuration:plugins</a>';
+$_lang['setting_tinymcerte.toolbar1'] = 'Панель инструментов 1';
+$_lang['setting_tinymcerte.toolbar1_desc'] = 'Этот параметр позволяет управлять тем, какие кнопки вы хотите отображать на панели инструментов. <a href="http://www.tinymce.com/wiki.php/Configuration:toolbar%3CN%3E">http://www.tinymce.com/wiki.php/Configuration:toolbar%3CN%3E</a>';
+$_lang['setting_tinymcerte.toolbar2'] = 'Панель инструментов 2';
+$_lang['setting_tinymcerte.toolbar2_desc'] = 'Этот параметр позволяет управлять тем, какие кнопки вы хотите отображать на панели инструментов. <a href="http://www.tinymce.com/wiki.php/Configuration:toolbar%3CN%3E">http://www.tinymce.com/wiki.php/Configuration:toolbar%3CN%3E</a>';
+$_lang['setting_tinymcerte.toolbar3'] = 'Панель инструментов 3';
+$_lang['setting_tinymcerte.toolbar3_desc'] = 'Этот параметр позволяет управлять тем, какие кнопки вы хотите отображать на панели инструментов. <a href="http://www.tinymce.com/wiki.php/Configuration:toolbar%3CN%3E">http://www.tinymce.com/wiki.php/Configuration:toolbar%3CN%3E</a>';
+$_lang['setting_tinymcerte.menubar'] = 'Строка меню';
+$_lang['setting_tinymcerte.menubar_desc'] = 'Эта опция позволяет вам настроить меню, которые вы хотите отображать в строке меню. <a href="http://www.tinymce.com/wiki.php/Configuration:menubar">http://www.tinymce.com/wiki.php/Configuration:menubar</a>';
+$_lang['setting_tinymcerte.statusbar'] = 'Статус бар';
+$_lang['setting_tinymcerte.statusbar_desc'] = 'Булевое значение, показать или скрыть строку состояния. <a href="http://www.tinymce.com/wiki.php/Configuration:statusbar">http://www.tinymce.com/wiki.php/Configuration:statusbar</a>';
+$_lang['setting_tinymcerte.image_advtab'] = 'Вкладка "Расширенные изображения"';
+$_lang['setting_tinymcerte.image_advtab_desc'] = 'Эта опция включает расширенную вкладку диалога изображений, позволяющую добавлять пользовательские стили к изображениям. <a href="http://www.tinymce.com/wiki.php/Configuration:image_advtab">http://www.tinymce.com/wiki.php/Configuration:image_advtab</a>';
+$_lang['setting_tinymcerte.object_resizing'] = 'Изменение размера объекта';
+$_lang['setting_tinymcerte.object_resizing_desc'] = 'Этот параметр позволяет включать / выключать маркеры изменения размера изображений, таблиц или мультимедийных объектов.. <a href="http://www.tinymce.com/wiki.php/Configuration:object_resizing">http://www.tinymce.com/wiki.php/Configuration:object_resizing</a>';
+$_lang['setting_tinymcerte.style_formats'] = 'Форматы стилей';
+$_lang['setting_tinymcerte.style_formats_desc'] = 'Этот параметр позволяет добавлять в редактор более продвинутые форматы стилей для текста и других элементов.. <a href="http://www.tinymce.com/wiki.php/Configuration:style_formats">http://www.tinymce.com/wiki.php/Configuration:style_formats</a>';
+$_lang['setting_tinymcerte.headers_format'] = 'Формат заголовков';
+$_lang['setting_tinymcerte.headers_format_desc'] = 'Этот параметр позволяет добавлять в редактор более продвинутые форматы стилей для текста и других элементов.. <a href="http://www.tinymce.com/wiki.php/Configuration:style_formats">http://www.tinymce.com/wiki.php/Configuration:style_formats</a>';
+$_lang['setting_tinymcerte.inline_format'] = 'Формат строчных элементов';
+$_lang['setting_tinymcerte.inline_format_desc'] = 'Этот параметр позволяет добавлять в редактор более продвинутые форматы стилей для текста и других элементов. <a href="http://www.tinymce.com/wiki.php/Configuration:style_formats">http://www.tinymce.com/wiki.php/Configuration:style_formats</a>';
+$_lang['setting_tinymcerte.blocks_format'] = 'Формат блоков';
+$_lang['setting_tinymcerte.blocks_format_desc'] = 'Этот параметр позволяет добавлять в редактор более продвинутые форматы стилей для текста и других элементов. <a href="http://www.tinymce.com/wiki.php/Configuration:style_formats">http://www.tinymce.com/wiki.php/Configuration:style_formats</a>';
+$_lang['setting_tinymcerte.alignment_format'] = 'Формат выравнивания';
+$_lang['setting_tinymcerte.alignment_format_desc'] = 'Этот параметр позволяет добавлять в редактор более продвинутые форматы стилей для текста и других элементов. <a href="http://www.tinymce.com/wiki.php/Configuration:style_formats">http://www.tinymce.com/wiki.php/Configuration:style_formats</a>';
+$_lang['setting_tinymcerte.paste_as_text'] = 'Вставить как текст';
+$_lang['setting_tinymcerte.paste_as_text_desc'] = 'Этот параметр позволяет установить состояние по умолчанию для пункта меню редактирования «вставить как текст». <a href="http://www.tinymce.com/wiki.php/Configuration:paste_as_text">http://www.tinymce.com/wiki.php/Configuration:paste_as_text</a>';
+$_lang['setting_tinymcerte.style_formats_merge'] = 'Объединение форматов стилей';
+$_lang['setting_tinymcerte.style_formats_merge_desc'] = 'Если вы хотите объединить ваши стили со стилем по умолчанию styles_format. <a href="http://www.tinymce.com/wiki.php/Configuration:style_formats">http://www.tinymce.com/wiki.php/Configuration:style_formats</a>';
+$_lang['setting_tinymcerte.link_class_list'] = 'Список классов для ссылок';
+$_lang['setting_tinymcerte.link_class_list_desc'] = 'Список классов для ссылок. <a href="http://www.tinymce.com/wiki.php/Configuration:link_class_list">http://www.tinymce.com/wiki.php/Configuration:link_class_list</a>';
+$_lang['setting_tinymcerte.browser_spellcheck'] = 'Проверка орфографии';
+$_lang['setting_tinymcerte.browser_spellcheck_desc'] = 'Включить или отключить проверку орфографии в браузере. <a href="http://www.tinymce.com/wiki.php/Configuration:browser_spellcheck">http://www.tinymce.com/wiki.php/Configuration:browser_spellcheck</a>';
+$_lang['setting_tinymcerte.content_css'] = 'CSS для контента';
+$_lang['setting_tinymcerte.content_css_desc'] = 'Загрузите дополнительные файлы CSS для Tiny MCE. Используйте "," (запятую) разделитель для нескольких файлов. <a href="http://www.tinymce.com/wiki.php/Configuration:content_css">http://www.tinymce.com/wiki.php/Configuration:content_css</a>';
+$_lang['setting_tinymcerte.image_class_list'] = 'Список классов для изображений';
+$_lang['setting_tinymcerte.image_class_list_desc'] = 'Список классов для изображений. <a href="http://www.tinymce.com/wiki.php/Configuration:image_class_list">http://www.tinymce.com/wiki.php/Configuration:image_class_list</a>';
+$_lang['setting_tinymcerte.external_config'] = 'Внешний конфиг';
+$_lang['setting_tinymcerte.external_config_desc'] = 'Путь к внешнему файлу конфигурации, который будет объединен со стандартными настройками системы. Файл конфигурации должен содержать действительный объект JSON.';
+$_lang['setting_tinymcerte.skin'] = 'Скин';
+$_lang['setting_tinymcerte.skin_desc'] = 'Выберите, какой скин использовать, он должен совпадать с именем скина. <a href="http://www.tinymce.com/wiki.php/Configuration:skin">http://www.tinymce.com/wiki.php/Configuration:skin</a>';
+$_lang['setting_tinymcerte.links_across_contexts'] = 'Ссылки в разных контекстах';
+$_lang['setting_tinymcerte.links_across_contexts_desc'] = 'Если этот параметр включен, при создании внутренних ссылок редактор будет перечислять ресурсы из других контекстов. Если выключено, отображаются только ресурсы из того же контекста.';
+$_lang['setting_tinymcerte.relative_urls'] = 'Относительные URL';
+$_lang['setting_tinymcerte.relative_urls_desc'] = 'Если для этого параметра установлено значение true, все URL-адреса, возвращаемые из MCFileManager, будут относительными из указанного document_base_url. Если установлено значение false, все URL будут преобразованы в абсолютные URL.';
+$_lang['setting_tinymcerte.remove_script_host'] = 'Удалить скрипт хоста';
+$_lang['setting_tinymcerte.remove_script_host_desc'] = 'Если эта опция включена, протокол и часть хоста URL-адресов, возвращаемых из MCFileManager, будут удалены. Этот параметр используется только в том случае, если для параметра relative_urls установлено значение false.';
+$_lang['setting_tinymcerte.valid_elements'] = 'Допустимые элементы';
+$_lang['setting_tinymcerte.valid_elements_desc'] = 'Определяет, какие элементы останутся в редактируемом тексте при сохранении редактора. Вы можете использовать этот параметр, чтобы ограничить возвращаемый HTML подмножеством, как описано ниже: <a href="https://www.tinymce.com/docs/configure/content-filtering/#valid_elements">https://www.tinymce.com/docs/configure/content-filtering/#valid_elements</a>.';
