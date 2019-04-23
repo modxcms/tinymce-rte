f4cead2e4988f3e7bde4beec18aa4bcc1d7006db
Sander Drenth <sander@sterc.nl>
2019-04-23 13:12:26 +0200

    Added option to retrieve a single resource

--- assets/components/tinymcerte/js/vendor/tinymce/plugins/modxlink/search.php
+++ assets/components/tinymcerte/js/vendor/tinymce/plugins/modxlink/search.php
@@ -4,10 +4,22 @@
  *
  * @package tinymce
  */
-
 require_once dirname(dirname(dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__))))))))).'/config.core.php';
 require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
 require_once MODX_CONNECTORS_PATH.'index.php';
+
+$id = $modx->getOption('id', $_REQUEST, '');
+if (!empty($id)) {
+    $resource = $modx->getObject('modResource', $id);
+    if ($resource) {
+        exit(json_encode([
+            'id'        => $resource->get('id'),
+            'pagetitle' => htmlspecialchars($resource->get('pagetitle')) . " (" . $resource->get('id') . ")",
+            'title'     => $resource->get('pagetitle'),
+            'alias'     => $resource->get('alias')
+        ]));
+    }
+}
 
 $query = $modx->getOption('q',$_REQUEST,'');
 if(strlen($query) < 3) exit();
@@ -26,14 +38,16 @@ $where_clause = array(array(
 ));
 
 if (!$across_contexts) {
-  if (isset($_GET['context']) and strlen($_GET['context']) and $_GET['context'] == 'undefined') {
+  if (isset($_GET['ctx']) and strlen($_GET['ctx'])) {
 	$where_clause[] = array(
-      'context_key' => $_GET['context']
+      'context_key' => $_GET['ctx']
 	);
   }
 }
 
 $c->where($where_clause);
+
+$count = $modx->getCount('modResource',$c);
 
 $c->select(array('id','pagetitle','alias'));
 $c->limit(10);
