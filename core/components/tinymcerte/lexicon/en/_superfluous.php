6b9e7701624e825fb3411727098fc890dba415d6
Thomas Jakobi <thomas.jakobi@partout.info>
2021-01-19 20:08:50 +0100

    Missing translation

--- core/components/tinymcerte/lexicon/en/_superfluous.php
+++
@@ -1,5 +0,0 @@
-<?php
-$_lang['tinymcerte'] = '';
-$_lang['area_default'] = '';
-$_lang['setting_tinymcerte.links_across_contexts'] = '';
-$_lang['setting_tinymcerte.links_across_contexts_desc'] = '';
