c1b91e9411ff2ce9072a1ea4f10b41b8f23ab29a
Thomas Jakobi <thomas.jakobi@partout.info>
2021-01-26 00:25:28 +0100

    New translations setting.inc.php (Russian)

--- core/components/tinymcerte/lexicon/ru/setting.inc.php
+++ core/components/tinymcerte/lexicon/ru/setting.inc.php
@@ -48,9 +48,9 @@ $_lang['setting_tinymcerte.menubar_desc'] = 'Это определяет, как
 $_lang['setting_tinymcerte.object_resizing'] = 'Изменение размера объекта';
 $_lang['setting_tinymcerte.object_resizing_desc'] = 'Эта опция позволяет включить/выключить обработку изменения размеров изображений, таблиц или медиа объектов. <a href="https://www.tiny.cloud/docs/configure/advanced-editing-behavior/#object_resizing">https://www.tiny.cloud/docs/configure/advanced-editing-behavior/#object_resizing</a>';
 $_lang['setting_tinymcerte.paste_as_text'] = 'Вставить как текст';
-$_lang['setting_tinymcerte.paste_as_text_desc'] = 'This option enables you to set the default state of the "paste as text" edit menu option. <a href="https://www.tiny.cloud/docs/plugins/opensource/paste/#paste_as_text">https://www.tiny.cloud/docs/plugins/opensource/paste/#paste_as_text</a>';
+$_lang['setting_tinymcerte.paste_as_text_desc'] = 'Этот параметр позволяет установить состояние по умолчанию для пункта меню редактирования «вставить как текст». <a href="https://www.tiny.cloud/docs-4x/plugins/paste/#paste_as_text">https://www.tiny.cloud/docs-4x/plugins/paste/#paste_as_text</a>';
 $_lang['setting_tinymcerte.plugins'] = 'Плагины';
-$_lang['setting_tinymcerte.plugins_desc'] = 'Set what plugins should be included, by default, NO plugins are loaded. <a href="https://www.tiny.cloud/docs/configure/integration-and-setup/#plugins">https://www.tiny.cloud/docs/configure/integration-and-setup/#plugins</a>';
+$_lang['setting_tinymcerte.plugins_desc'] = 'Укажите, какие плагины должны быть включены, по умолчанию НЕ загружаются плагины. <a href="https://www.tiny.cloud/docs/configure/integration-and-setup/#plugins">https://www.tiny.cloud/docs/configure/integration-and-setup/#plugins</a>';
 $_lang['setting_tinymcerte.relative_urls'] = 'Относительные URL';
 $_lang['setting_tinymcerte.relative_urls_desc'] = 'Если для этого параметра установлено значение true, все URL-адреса, возвращаемые из MCFileManager, будут относительными из указанного document_base_url. Если установлено значение false, все URL будут преобразованы в абсолютные URL.';
 $_lang['setting_tinymcerte.remove_script_host'] = 'Удалить скрипт хоста';
@@ -58,8 +58,8 @@ $_lang['setting_tinymcerte.remove_script_host_desc'] = 'Если эта опци
 $_lang['setting_tinymcerte.settings'] = 'Дополнительные настройки';
 $_lang['setting_tinymcerte.settings_desc'] = 'JSON кодированный массив дополнительных настроек TinyMCE.';
 $_lang['setting_tinymcerte.skin'] = 'Скин';
-$_lang['setting_tinymcerte.skin_desc'] = 'This option allows you to specify the skin that TinyMCE should use. <a href="https://www.tiny.cloud/docs/configure/editor-appearance/#skin">https://www.tiny.cloud/docs/configure/editor-appearance/#skin</a>';
+$_lang['setting_tinymcerte.skin_desc'] = 'Эта опция позволяет указать скин, который должен использовать TinyMCE. <a href="https://www.tiny.cloud/docs/configure/editor-appearance/#skin">https://www.tiny.cloud/docs/configure/editor-appearance/#skin</a>';
 $_lang['setting_tinymcerte.statusbar'] = 'Статус бар';
-$_lang['setting_tinymcerte.statusbar_desc'] = 'This option allows you to specify whether TinyMCE should display the status bar at the bottom of the editor. <a href="https://www.tiny.cloud/docs/configure/editor-appearance/#statusbar">https://www.tiny.cloud/docs/configure/editor-appearance/#statusbar</a>';
+$_lang['setting_tinymcerte.statusbar_desc'] = 'Эта опция позволяет указать, показывать ли TinyMCE строку состояния внизу редактора. <a href="https://www.tiny.cloud/docs/configure/editor-appearance/#statusbar">https://www.tiny.cloud/docs/configure/editor-appearance/#statusbar</a>';
 $_lang['setting_tinymcerte.valid_elements'] = 'Допустимые элементы';
-$_lang['setting_tinymcerte.valid_elements_desc'] = 'This option defines which elements will remain in the edited text when the editor saves. You can use this to limit the returned HTML to a subset. <a href="https://www.tiny.cloud/docs/configure/content-filtering/#extended_valid_elements">https://www.tiny.cloud/docs/configure/content-filtering/#extended_valid_elements</a>.';
+$_lang['setting_tinymcerte.valid_elements_desc'] = 'Эта опция определяет, какие элементы останутся в редактируемом тексте при сохранении редактора. Вы можете использовать это для ограничения возвращаемого HTML подмножества. <a href="https://www.tiny.cloud/docs/configure/content-filtering/#extended_valid_elements">https://www.tiny.cloud/docs/configure/content-filtering/#extended_valid_elements</a>.';
