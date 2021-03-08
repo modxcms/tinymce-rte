e9cf5dbba739a11bf41ea3ae648edc4a35d694ef
Thomas Jakobi <thomas.jakobi@partout.info>
2021-03-08 17:29:43 +0100

    Fix resetting the which_editor system setting during uninstall the package

--- _build/resolvers/resolve.whicheditor.php
+++ _build/resolvers/resolve.whicheditor.php
@@ -13,7 +13,9 @@ if ($object->xpdo) {
         case xPDOTransport::ACTION_INSTALL:
         case xPDOTransport::ACTION_UPGRADE:
             $object->xpdo->log(xPDO::LOG_LEVEL_INFO, 'Attempting to set which_editor setting to TinyMCE RTE.');
-            $setting = $object->xpdo->getObject('modSystemSetting', ['key' => 'which_editor']);
+            $setting = $object->xpdo->getObject('modSystemSetting', [
+                'key' => 'which_editor'
+            ]);
             if ($setting) {
                 $setting->set('value', 'TinyMCE RTE');
                 $setting->save();
@@ -21,7 +23,10 @@ if ($object->xpdo) {
             unset($setting);
 
             $object->xpdo->log(xPDO::LOG_LEVEL_INFO, 'Attempting to set use_editor setting to on.');
-            $setting = $object->xpdo->getObject('modSystemSetting', ['key' => 'use_editor']);
+            $setting = $object->xpdo->getObject('modSystemSetting', [
+                'key' => 'use_editor',
+                'value' => '0'
+            ]);
             if ($setting) {
                 $setting->set('value', 1);
                 $setting->save();
@@ -30,15 +35,12 @@ if ($object->xpdo) {
 
             break;
         case xPDOTransport::ACTION_UNINSTALL:
-            $setting = $object->xpdo->getObject('modSystemSetting', ['key' => 'which_editor']);
+            $setting = $object->xpdo->getObject('modSystemSetting', [
+                'key' => 'which_editor',
+                'value' => 'TinyMCE RTE'
+            ]);
             if ($setting) {
                 $setting->set('value', '');
-                $setting->save();
-            }
-
-            $setting = $object->xpdo->getObject('modSystemSetting', ['key' => 'use_editor']);
-            if ($setting) {
-                $setting->set('value', 0);
                 $setting->save();
             }
 
