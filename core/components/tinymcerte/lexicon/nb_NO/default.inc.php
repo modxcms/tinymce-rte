23dc809069b991670b01dc238a18825decff47cc
Allan Nordhøy <epost@anotheragency.no>
2021-03-16 21:30:17 +0100

    Added translation using Weblate (Norwegian Bokmål)

---
+++ core/components/tinymcerte/lexicon/nb_NO/default.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Default Lexicon Entries for TinyMCE Rich Text Editor
+ *
+ * @package tinymcerte
+ * @subpackage lexicon
+ */
+$_lang['tinymcerte'] = 'TinyMCE Rich Text Editor';
+$_lang['tinymcerte.float_left'] = 'Left';
+$_lang['tinymcerte.float_none'] = 'None';
+$_lang['tinymcerte.float_right'] = 'Right';
+$_lang['tinymcerte.select_none'] = '(None)';
+$_lang['tinymcerte.select_resource'] = 'Select resource';
